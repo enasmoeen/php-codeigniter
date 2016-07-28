f867ac3ba811cdd05fa9da8ff902618dac0e4bcd
enasmoeen <enasmoeenmashaqi@gmail.com>
2016-07-28 14:19:53 +0300

    Add files via upload

---
+++ application/views/hello_view.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+</head>
+<body>
+<h2>Hello</h2>
+<h6>If you want to create an article
+<a href="http://localhost/CodeIgniter-3.0.6/index.php/article_ctrl">click here</a>
+</body>
+</html>
